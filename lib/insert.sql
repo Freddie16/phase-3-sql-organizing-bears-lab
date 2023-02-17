ec7f8bca90f56ba73c2b582414479b8594fdd692
Fred Murigi <freddiemurigi@gmail.com>
2023-02-17 10:35:41 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("Mr.Chocolate",25,"M","Black","Cool",TRUE);
+iNSERT INTO(name,age,sex,color,temperament,alive) VALUES("Rowdy",25,"M","Black","Cool",TRUE);
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("Tabitha",25,"F","Black","Cool",TRUE);
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",25,"F","Black","Cool",TRUE);
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("Melissa",25,"F","Black","Cool",TRUE);
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("Grinch",25,"M","Black","Cool",TRUE);
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("Wendy",25,"F","Black","Cool",TRUE);
+INSERT INTO(name,age,sex,color,temperament,alive) VALUES("NULL",25,"F","Black","Cool",TRUE);
